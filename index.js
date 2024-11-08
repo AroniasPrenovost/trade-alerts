c8eded5a05780a1996bf806817ddf54097f135f5
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2024-11-08 08:05:46 -0800

    incorporating maker and taker fees

--- index.js
+++ index.js
@@ -89,10 +89,11 @@ const SPOT_MAKER_FEE = Number(process.env.SPOT_MAKER_FEE);
 const SPOT_TAKER_FEE = Number(process.env.SPOT_TAKER_FEE);
 
 const ASSET_LIST = [
-  { symbol: 'AVAX', support: 24, resistance: 29, entry: 25.49, sellLimit: 29, shares: 0 },
+  { symbol: 'AVAX', support: 24, resistance: 29, entry: 24, sellLimit: 29, shares: 0 },
   { symbol: 'DOT', support: 3, resistance: 4.5, entry: 3.873, sellLimit: 4.40, shares: 12.99331849 },
-  { symbol: 'UNI', support: 7.40, resistance: 9.0, entry: 0, sellLimit: 0, shares: 0 },
-  // { symbol: 'ADA', support: .33, resistance: .35, entry: .33, sellLimit: .36, shares: 0 },
+  // { symbol: 'UNI', support: 7.40, resistance: 9.0, entry: 0, sellLimit: 0, shares: 0 },
+    { symbol: 'CRO', support: .086, resistance: .1, entry: .085, sellLimit: .1, shares: 0 },
+  { symbol: 'ADA', support: .38, resistance: .45, entry: .38, sellLimit: .40, shares: 200 },
 ];
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
